91013917be44d1fbaed331f2096a757ec19b42ab
ElSombrero2 <nirilala15@gmail.com>
2026-02-28 20:14:05 +0100

    feat: add taskify

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,8 +3,12 @@ use std::env::home_dir;
 use stree::config;
 
 fn main() {
+    let default_config_file = "/.config/stree/config.toml";
+
     if let Some(dir) = home_dir() {
-        let config = config::Config::load(String::from(dir.to_str().unwrap()));
+        let config_dir =  dir.to_str().unwrap().to_owned() + default_config_file;
+        println!("{config_dir}");
+        let config = config::Config::load(config_dir);
         dbg!(config);
     }
    
--- stree/src/config/mod.rs
+++ stree/src/config/mod.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs::{self, create_dir}, path::Path};
 
 use crate::config::credential::Credential;
 use serde::{Deserialize, Serialize};
@@ -7,28 +7,34 @@ mod credential;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
-    filename: String,
     endpoint: Option<String>,
     credential: Option<Credential>,
+    bucket: Option<String>,
 }
 
 impl Config {
     pub fn load(path: String) -> Config {
-        let filedir = format!("{}{}", path, "/.config/stree.toml");
-        if fs::exists(&filedir).unwrap() {
-           let content = fs::read_to_string(filedir).unwrap();
+        if fs::exists(&path).unwrap() {
+           let content = fs::read_to_string(path).unwrap();
            return toml::from_str::<Config>(&content).unwrap();
         }
         let default = Config {
-            filename: String::from(&filedir),
             endpoint: None,
             credential: None,
+            bucket: None,
         };
-        default.save();        
+        default.save(String::from(&path)).unwrap();        
         default
     }
 
-    pub fn save(self: &Self) {
-        fs::write(&self.filename, toml::to_string(self).unwrap()).unwrap();
+    pub fn save(&self, filename: String) -> Result<(), String> {
+        let path = Path::new(&filename);
+        let parent = path.parent().unwrap();
+        if !parent.exists() && create_dir(parent).is_err() {
+            // [TODO]: Add good error handling
+            return Err(String::from("An error occurred"));
+        }
+        fs::write(path, toml::to_string(self).unwrap()).unwrap();
+        Ok(())
     }
 }
