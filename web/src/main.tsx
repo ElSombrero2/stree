17b7c7c237e80f394a8346a17a14a4cc8e52ac01
ElSombrero <nirilala15@gmail.com>
2026-04-26 00:39:44 +0200

    add atomic design structure

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,22 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
+import { router } from './components/router'
+import { Header } from './components/organisms/header/Header'
 import './index.css'
-import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/test',
-		element: <div>Hello World Test</div>
-	}
-])
+function App() {
+  return (
+    <>
+			<Header />
+			<RouterProvider router={router} />
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
